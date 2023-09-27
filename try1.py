f1949fe1c20611af3673d14ee5802f6b8f82feb8
AbiMathav19 <145982404+AbiMathav19@users.noreply.github.com>
2023-09-27 22:59:50 +0530

    Update try1.py

--- try1.py
+++ try1.py
@@ -5,8 +5,8 @@ import requests
 import biotite.structure.io as bsio
 
 #st.set_page_config(layout = 'wide')
-st.sidebar.title('🎈 ESMFold')
-st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
+st.sidebar.title(' PROStruct Predictor')
+st.sidebar.write('[*PROStruct*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 # stmol
 def render_mol(pdb):
     pdbview = py3Dmol.view()
@@ -27,14 +27,14 @@ def update(sequence=txt):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
-    response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', headers=headers, data=sequence)
+    response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/',headers=headers,data=sequence)
     name = sequence[:3] + sequence[-3:]
     pdb_string = response.content.decode('utf-8')
 
-    with open('predicted.pdb', 'w') as f:
+    with open('predicted.pdb','w') as f:
         f.write(pdb_string)
 
-    struct = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
+    struct = bsio.load_structure('predicted.pdb',extra_fields=["b_factor"])
     b_value = round(struct.b_factor.mean(), 4)
 
     # Display protein structure
@@ -53,7 +53,7 @@ def update(sequence=txt):
         mime='text/plain',
     )
 
-predict = st.sidebar.button('Predict', on_click=update)
+predict = st.sidebar.button('Predict',on_click=update)
 
 
 if not predict:
