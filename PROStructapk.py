82ff52122e5d2fbbd0681912e856cd83c008e6d4
AbiMathav19 <145982404+AbiMathav19@users.noreply.github.com>
2023-10-29 10:45:06 +0530

    Update PROStructapk.py

--- PROStructapk.py
+++ PROStructapk.py
@@ -6,7 +6,6 @@ import biotite.structure.io as bsio
 
 #st.set_page_config(layout = 'wide')
 st.sidebar.title(' PROStruct-Predictor')
-st.sidebar.write('made with [streamlit](https://streamlit.io/)')
 st.sidebar.write('[*PROStruct-Predictor*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 # stmol
 def render_mol(pdb):
@@ -48,7 +47,7 @@ def update(sequence=txt):
     st.info(f'plDDT: {b_value}')
 
     st.download_button(
-        label="Download PDB",
+        label="Download PDB File",
         data=pdb_string,
         file_name='predicted.pdb',
         mime='text/plain',
